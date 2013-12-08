3e8b0cc21d67a67de117e0eae5bd2bd03a73428a
Jonah Grant <jonah@jonahgrant.com>
2013-12-08 16:00:56 -0600

    nevermind

--- Fare/Source/UMNetworkingSession.m
+++ Fare/Source/UMNetworkingSession.m
@@ -35,92 +35,46 @@
     return [kUMRootURL stringByAppendingString:path];
 }
 
-- (void)GET:(NSString *)getEndpoint
- parameters:(NSDictionary *)parameters
-    success:(void (^)(AFHTTPRequestOperation *, id))success
-    failure:(void (^)(AFHTTPRequestOperation *, id))failure
- retryCount:(NSUInteger)retry
-serializer:(AFHTTPResponseSerializer *)serializer {
-    
-    __block AFHTTPRequestOperation *_operation;
-    __block NSError *_error;
-    
-    if (retry <= 0) {
-        if (failure) {
-            failure(_operation, _error);
-        }
-    } else {
-        [[self.manager GET:getEndpoint
-                parameters:parameters
-                   success:^(AFHTTPRequestOperation *operation, id responseObject) {
-                       success(operation, responseObject);
-                   } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-                       NSLog(@"failed, trying again");
-                       
-                       _operation = operation;
-                       _error = error;
-                       [self GET:getEndpoint
-                      parameters:parameters
-                         success:success
-                         failure:failure
-                      retryCount:retry - 1
-                      serializer:serializer];
-                   }] setResponseSerializer:serializer];
-    }
-    
-}
-
 - (void)fetchArrivalsWithSuccessBlock:(UMArrayBlock)successBlock errorBlock:(UMErrorBlock)errorBlock {
-    [self GET:[self rootURLWithPath:kUMAPIFetchArrivals]
-   parameters:nil
-      success:HandlerBlock(successBlock)
-      failure:HandlerBlock(errorBlock)
-   retryCount:self.retryCount
-   serializer:[Arrival um_xmlArrayResponseSerializer]];
+    [[self.manager GET:[self rootURLWithPath:kUMAPIFetchArrivals] parameters:nil
+               success:HandlerBlock(successBlock)
+               failure:HandlerBlock(errorBlock)]
+     setResponseSerializer:[Arrival um_xmlArrayResponseSerializer]];
 }
 
 - (void)fetchBusesWithSuccessBlock:(UMArrayBlock)successBlock errorBlock:(UMErrorBlock)errorBlock {
-    [self GET:[self rootURLWithPath:kUMAPIFetchBuses] parameters:nil
-      success:HandlerBlock(successBlock)
-      failure:HandlerBlock(errorBlock)
-   retryCount:self.retryCount
-   serializer:[Bus um_jsonArrayResponseSerializer]];
+    [[self.manager GET:[self rootURLWithPath:kUMAPIFetchBuses] parameters:nil
+               success:HandlerBlock(successBlock)
+               failure:HandlerBlock(errorBlock)]
+     setResponseSerializer:[Bus um_jsonArrayResponseSerializer]];
 }
 
 - (void)fetchAnnouncementsWithSuccessBlock:(UMArrayBlock)successBlock errorBlock:(UMErrorBlock)errorBlock {
-    [self GET:[self rootURLWithPath:kUMAPIFetchAnnouncements]
-   parameters:nil
-      success:HandlerBlock(successBlock)
-      failure:HandlerBlock(errorBlock)
-   retryCount:self.retryCount
-   serializer:[Announcement um_jsonArrayResponseSerializer]];
+    [[self.manager GET:[self rootURLWithPath:kUMAPIFetchAnnouncements] parameters:nil
+               success:HandlerBlock(successBlock)
+               failure:HandlerBlock(errorBlock)]
+     setResponseSerializer:[Announcement um_jsonArrayResponseSerializer]];
 }
 
 - (void)fetchRoutesWithSuccessBlock:(UMArrayBlock)successBlock errorBlock:(UMErrorBlock)errorBlock {
-    [self GET:[self rootURLWithPath:kUMAPIFetchRoutes]
-   parameters:nil
-      success:HandlerBlock(successBlock)
-      failure:HandlerBlock(errorBlock)
-   retryCount:self.retryCount
-   serializer:[Route um_jsonArrayResponseSerializer]];
+    [[self.manager GET:[self rootURLWithPath:kUMAPIFetchRoutes] parameters:nil
+               success:HandlerBlock(successBlock)
+               failure:HandlerBlock(errorBlock)]
+     setResponseSerializer:[Route um_jsonArrayResponseSerializer]];
 }
 
 - (void)fetchStopsWithSuccessBlock:(UMArrayBlock)successBlock errorBlock:(UMErrorBlock)errorBlock {
-    [self GET:[self rootURLWithPath:kUMAPIFetchStops]
-   parameters:nil
-      success:HandlerBlock(successBlock)
-      failure:HandlerBlock(errorBlock)
-   retryCount:self.retryCount
-   serializer:[Stop um_jsonArrayResponseSerializer]];
+    [[self.manager GET:[self rootURLWithPath:kUMAPIFetchStops] parameters:nil
+               success:HandlerBlock(successBlock)
+               failure:HandlerBlock(errorBlock)]
+     setResponseSerializer:[Stop um_jsonArrayResponseSerializer]];
 }
 
 - (void)fetchTraceRouteForRouteID:(NSString *)routeID withSuccessBlock:(UMArrayBlock)successBlock errorBlock:(UMErrorBlock)errorBlock {
-    [self GET:[self rootURLWithPath:[NSString stringWithFormat:kUMAPIFetchTraceRoute, routeID]]
-   parameters:nil
-      success:HandlerBlock(successBlock)
-      failure:HandlerBlock(errorBlock)
-   retryCount:self.retryCount
-   serializer:[TraceRoute um_xmlArrayResponseSerializer]];
+    [[self.manager GET:[self rootURLWithPath:[NSString stringWithFormat:kUMAPIFetchTraceRoute, routeID]] parameters:nil
+               success:HandlerBlock(successBlock)
+               failure:HandlerBlock(errorBlock)]
+     setResponseSerializer:[TraceRoute um_xmlArrayResponseSerializer]];
 }
 
 @end
